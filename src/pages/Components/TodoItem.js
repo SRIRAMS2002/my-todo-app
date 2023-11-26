21041da12e9940f113534dd546d400ad736d5a17
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-11-26 10:19:06 +0530

    update-3

--- src/pages/Components/TodoItem.js
+++ src/pages/Components/TodoItem.js
@@ -4,8 +4,8 @@ const TodoItem = ({ task, handleDelete }) => {
   return (
     <div className="flex items-center justify-between p-2 border-b">
       <div>
-        <p className="font-medium text-xl">{task.title}</p>
-        <p className="text-md text-gray-400">{task.description}</p>
+        <p className="font-medium">{task.title}</p>
+        <p className="text-sm text-gray-500">{task.description}</p>
       </div>
       <button onClick={() => handleDelete(task.id)} className="text-red-500 hover:text-red-700">
         Delete
