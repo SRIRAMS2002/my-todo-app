cf275d02238c909bafecc99d51bda016b75b5ae6
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-11-26 10:57:18 +0530

    update-7

--- src/pages/Components/TodoItem.js
+++ src/pages/Components/TodoItem.js
@@ -1,34 +1,30 @@
-import React from 'react';
+import React from "react";
 
+const TodoItem = ({ task, handleDelete }) => {
+  if (!task || !task.title) {
+    return (
+      <div className="flex items-center justify-between p-2 border-b">
+        <p className="text-red-500">Invalid Task</p>
+      </div>
+    );
+  }
 
-
-
-
-
-
-
-
-    const TodoItem = ({ task, handleDelete }) => {
-        if (!task || !task.title) {
-          return null; // Or display an error message
-        }
   return (
-    
-        
     <div className="flex items-center justify-between p-2 border-b">
-       
       <div>
         <p className="font-medium">{task.title}</p>
-        <p className="text-sm text-gray-500">{task.description}</p>
+        {task.description && (
+          <p className="text-sm text-gray-500">{task.description}</p>
+        )}
       </div>
-      <button onClick={() => handleDelete(task.id)} className="text-red-500 hover:text-red-700">
+      <button
+        onClick={() => handleDelete(task.id)}
+        className="text-red-500 hover:text-red-700"
+      >
         Delete
       </button>
     </div>
- 
-  )
-}
+  );
+};
 
-export default TodoItem
-
-
+export default TodoItem;
